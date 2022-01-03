c9da5349d395ea2b35c2527063735f74e4884d2e
Peter Hutchison <pjhutch17@talktalk.net>
2022-01-03 09:51:09 +0000

    Upload project files

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Number_Plate_Decoder_2019
+{
+    partial class frmNumberPlate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPlate = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmdDecode = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtOffice = new System.Windows.Forms.TextBox();
+            this.txtLocal = new System.Windows.Forms.TextBox();
+            this.txtMonth = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Number Plate";
+            // 
+            // txtPlate
+            // 
+            this.txtPlate.Location = new System.Drawing.Point(140, 8);
+            this.txtPlate.Name = "txtPlate";
+            this.txtPlate.Size = new System.Drawing.Size(144, 20);
+            this.txtPlate.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "DVLA Office";
+            // 
+            // cmdDecode
+            // 
+            this.cmdDecode.Location = new System.Drawing.Point(96, 72);
+            this.cmdDecode.Name = "cmdDecode";
+            this.cmdDecode.Size = new System.Drawing.Size(136, 24);
+            this.cmdDecode.TabIndex = 3;
+            this.cmdDecode.Text = "Decode";
+            this.cmdDecode.UseVisualStyleBackColor = true;
+            this.cmdDecode.Click += new System.EventHandler(this.cmdDecode_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Local Identifier";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Reg Month";
+            // 
+            // txtOffice
+            // 
+            this.txtOffice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtOffice.Location = new System.Drawing.Point(152, 128);
+            this.txtOffice.Name = "txtOffice";
+            this.txtOffice.Size = new System.Drawing.Size(152, 20);
+            this.txtOffice.TabIndex = 6;
+            // 
+            // txtLocal
+            // 
+            this.txtLocal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLocal.Location = new System.Drawing.Point(152, 168);
+            this.txtLocal.Name = "txtLocal";
+            this.txtLocal.Size = new System.Drawing.Size(152, 20);
+            this.txtLocal.TabIndex = 7;
+            // 
+            // txtMonth
+            // 
+            this.txtMonth.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtMonth.Location = new System.Drawing.Point(152, 208);
+            this.txtMonth.Name = "txtMonth";
+            this.txtMonth.Size = new System.Drawing.Size(152, 20);
+            this.txtMonth.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 248);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Reg Year";
+            // 
+            // txtYear
+            // 
+            this.txtYear.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtYear.Location = new System.Drawing.Point(152, 248);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(152, 20);
+            this.txtYear.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 288);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Car Number";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(152, 288);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(148, 20);
+            this.txtNumber.TabIndex = 12;
+            // 
+            // frmNumberPlate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 320);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtMonth);
+            this.Controls.Add(this.txtLocal);
+            this.Controls.Add(this.txtOffice);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmdDecode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPlate);
+            this.Controls.Add(this.label1);
+            this.Name = "frmNumberPlate";
+            this.Text = "Number Plate Decoder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPlate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button cmdDecode;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtOffice;
+        private System.Windows.Forms.TextBox txtLocal;
+        private System.Windows.Forms.TextBox txtMonth;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNumber;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,323 @@
+﻿// Number Plate Decoder
+// by PJ Hutchison
+// Aug 2019.
+// Changes
+// 03/01/2022 - Fixed Reg. Year field.
+
+using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Number_Plate_Decoder_2019
+{
+    public partial class frmNumberPlate : Form
+    {
+        public frmNumberPlate()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Decode given car number.
+        /// </summary>
+        /// <param name="sender">Sender object</param>
+        /// <param name="e">Event</param>
+        private void cmdDecode_Click(object sender, EventArgs e)
+        {
+            Decode_CarNumber();
+        }
+
+        /// <summary>
+        /// Decode old or new style car number.
+        /// </summary>
+        void Decode_CarNumber()
+        {
+            String Office, Local, RegMonth, RegYear, CarNum, Plate;
+            String OfficeName, Ids, MonthName, FullYear;
+            int MonthNum;
+
+            Ids = "ABCDEFGHJKLMNOPRSTUVWXY"; // q,i,z are not included
+            OfficeName = "";
+            // Get data from Plate number
+            Plate = txtPlate.Text.ToUpper();
+            Office = Plate.Substring(0, 1);
+            Local = Plate.Substring(1, 1);
+            RegMonth = Plate.Substring(2, 1);
+            RegYear = Plate.Substring(3, 1);
+            CarNum = Plate.Substring(5, 3);
+            // Validation
+            if (Plate.Length != 8)
+            {
+                txtOffice.Text = "Old Number Plate";
+                txtYear.Text = Get_Old_RegYear(Plate.Substring(0, 1));
+                return;
+            }
+            if (Ids.IndexOf(Office) == 9 & Ids.IndexOf(Local) == 0)
+            {
+                txtOffice.Text = "Old Number Plate";
+                txtYear.Text = Get_Old_RegYear(Plate.Substring(0, 1));
+                return;
+            }
+            if (String.Compare(RegMonth, "0") < 0 | String.Compare(RegMonth, "9") > 0)
+            {
+                txtOffice.Text = "Old Number Plate";
+                txtYear.Text = Get_Old_RegYear(Plate.Substring(0, 1));
+                return;
+            }
+            if (String.Compare(Office, "A") >= 0 && String.Compare(Office, "Z") <= 0 && String.Compare(Local, "0") >= 0 && String.Compare(Local, "9") <= 0)
+            {
+                txtOffice.Text = "Old Number Plate";
+                txtYear.Text = Get_Old_RegYear(Plate.Substring(0, 1));
+                return;
+            }
+            // Decode DVLA Office
+            switch (Office)
+            {
+                case "A":
+                    if (Ids.Substring(0, 13).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Peterborough";
+                    }
+                    else if (Ids.Substring(13, 6).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Norwich";
+                    }
+                    else if (Ids.Substring(19, 4).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Ipswich";
+                    }
+                    break;
+                case "B":
+                    OfficeName = "Birmingham";
+                    break;
+                case "C":
+                    if (Ids.Substring(0, 14).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Cardiff";
+                    }
+                    else if (Ids.Substring(14, 6).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Swansea";
+                    }
+                    else if (Ids.Substring(20, 3).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Bangor";
+                    }
+                    break;
+                case "D":
+                    if (Ids.Substring(0, 10).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Chester";
+                    }
+                    else if (Ids.Substring(10, 13).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Shewsbury";
+                    }
+                    break;
+                case "E":
+                    OfficeName = "Chemsford";
+                    break;
+                case "F":
+                    if (Ids.Substring(0, 15).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Nottingham";
+                    }
+                    else if (Ids.Substring(15, 7).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Lincoln";
+                    }
+                    break;
+                case "G":
+                    if (Ids.Substring(0, 14).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Maidstone";
+                    }
+                    else if (Ids.Substring(14, 7).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Brighton";
+                    }
+                    break;
+                case "H":
+                    if (Ids.Substring(0, 9).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "BourneMouth";
+                    }
+                    else if (Ids.Substring(9, 13).IndexOf(Local) > 0 && Local != "W")
+                    {
+                        OfficeName = "Portsmouth";
+                    }
+                    else
+                        OfficeName = "Isle of Wight"; // W number
+                    break;
+                case "K":
+                    if (Ids.Substring(0, 11).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Bournemouth"; // Formally Luton
+                    }
+                    else if (Ids.Substring(11, 12).IndexOf(Local) > 0 && Local != "W")
+                    {
+                        OfficeName = "Northampton";
+                    }
+                    break;
+                case "L":
+                    if (Ids.Substring(0, 9).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Wimbledon";
+                    }
+                    else if (Ids.Substring(9, 9).IndexOf(Local) > 0 && Local != "W")
+                    {
+                        OfficeName = "Borehamwood"; // Formally Stanmore
+                    }
+                    else if (Ids.Substring(18, 5).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Sidcup";
+                    }
+                    break;
+                case "M":
+                    OfficeName = "Manchester";
+                    break;
+                case "N":
+                    if (Ids.Substring(0, 13).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Newcastle";
+                    }
+                    else if (Ids.Substring(13, 9).IndexOf(Local) > 0 && Local != "W")
+                    {
+                        OfficeName = "Stockton";
+                    }
+                    break;
+                case "O":
+                    OfficeName = "Oxford";
+                    break;
+                case "P":
+                    if (Ids.Substring(0, 18).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Preston";
+                    }
+                    else if (Ids.Substring(18, 5).IndexOf(Local) > 0 && Local != "W")
+                    {
+                        OfficeName = "Carlisle";
+                    }
+                    break;
+                case "R":
+                    OfficeName = "Theale"; // in Reading
+                    break;
+                case "S":
+                    if (Ids.Substring(0, 9).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Glasgow";
+                    }
+                    else if (Ids.Substring(9, 5).IndexOf(Local) > 0 && Local != "W")
+                    {
+                        OfficeName = "Edinburgh";
+                    }
+                    else if (Ids.Substring(14, 3).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Dundee";
+                    }
+                    else if (Ids.Substring(18, 3).IndexOf(Local) > 0 && Local != "W")
+                    {
+                        OfficeName = "Aberdeen";
+                    }
+                    else if (Ids.Substring(21, 3).IndexOf(Local) > 0 && Local != "W")
+                    {
+                        OfficeName = "Inverness";
+                    }
+                    break;
+                case "V":
+                    OfficeName = "Worcester";
+                    break;
+                case "W":
+                    if (Ids.Substring(0, 9).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Exeter";
+                    }
+                    else if (Ids.Substring(9, 2).IndexOf(Local) > 0 && Local != "W")
+                    {
+                        OfficeName = "Truro";
+                    }
+                    else if (Ids.Substring(11, 12).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Bristol";
+                    }
+                    break;
+                case "Y":
+                    if (Ids.Substring(0, 11).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Leeds";
+                    }
+                    else if (Ids.Substring(11, 9).IndexOf(Local) > 0 && Local != "W")
+                    {
+                        OfficeName = "Sheffield";
+                    }
+                    else if (Ids.Substring(20, 3).IndexOf(Local) > 0)
+                    {
+                        OfficeName = "Barnsley";
+                    }
+                    break;
+                default:
+                    OfficeName = "Invalid Office";
+                    break;
+            }
+            if (String.Compare(Local, "Q") == 0 || String.Compare(Local, "I") == 0 || String.Compare(Local, "Z") == 0)
+            {
+                OfficeName = "Invalid Office";
+            }
+            // Work out month of registration
+            MonthNum = Int32.Parse(RegMonth); // String to integer
+            if (MonthNum < 5)
+                MonthName = "March";
+            else
+                MonthName = "September";
+            // Work out year of registration
+            if (MonthNum < 5)
+            { 
+                FullYear = String.Concat("20", RegMonth, RegYear);
+            }
+            else
+            {
+                RegMonth = (MonthNum - 5).ToString();
+                FullYear = String.Concat("20", RegMonth, RegYear);
+            }
+            // Display results on form
+            txtOffice.Text = OfficeName;
+            txtLocal.Text = Local;
+            txtMonth.Text = MonthName;
+            txtYear.Text = FullYear;
+            txtNumber.Text = CarNum;
+        }
+
+
+        /// <summary>
+        /// Works out Reg Year for previous number plates. e.g. L25 LUM = 1983 + 1993
+        /// </summary>
+        /// <param name="YearLetter">Year letter. NB: I, O, Q and Z are not included (similar to 1, 0 and 2)</param>
+        /// <returns>Old Year</returns>
+        String Get_Old_RegYear(String YearLetter)
+        {
+            Int32 RegYear = 0;
+            byte yr;
+
+            if (String.Compare(YearLetter, "A") > 0 && String.Compare(YearLetter, "S") < 0)
+            {
+                yr = Encoding.ASCII.GetBytes(YearLetter)[0];
+                RegYear = 1983 + yr - 65;
+                if (String.Compare(YearLetter, "I") > 0)
+                    RegYear = RegYear - 1;
+                if (String.Compare(YearLetter, "O") > 0)
+                    RegYear = RegYear - 1;
+                if (String.Compare(YearLetter, "Q") > 0)
+                    RegYear = RegYear - 1;
+            }
+            else if (String.Compare(YearLetter, "T") == 0 || String.Compare(YearLetter, "V") == 0)
+                RegYear = 1999;
+            else if (String.Compare(YearLetter, "W") == 0 || String.Compare(YearLetter, "X") == 0)
+                RegYear = 2000;
+            else if (String.Compare(YearLetter, "Y") == 0)
+                RegYear = 2001;
+
+            return (RegYear.ToString());
+        }
+    
+    }
+}
